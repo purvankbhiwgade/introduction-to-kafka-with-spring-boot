c2a35922d3fb0f08a4bdae4216e0841280184465
Purvank Bhiwgade <bhiwgadepurvank@gmail.com>
2024-07-24 14:55:31 +0530

    JSON Deserializer

--- src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -1,8 +1,12 @@
 package dev.lydtech.dispatch.handler;
 
+import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.service.DispatchService;
+import dev.lydtech.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -12,16 +16,19 @@ class OrderCreatedHandlerTest {
 
     private OrderCreatedHandler handler;
     private DispatchService dispatchServiceMock;
+    private OrderCreated orderCreated;
 
     @BeforeEach
     void setUp() {
         dispatchServiceMock = mock(DispatchService.class);
         handler = new OrderCreatedHandler(dispatchServiceMock);
+        orderCreated = new OrderCreated();
     }
 
     @Test
     void listen() {
-        handler.listen("payload");
-        verify(dispatchServiceMock, times(1)).process("payload");
+        OrderCreated testEvent = TestEventData.builderOrderCreatedEvent(UUID.randomUUID(), UUID.randomUUID().toString());
+        handler.listen(testEvent);
+        verify(dispatchServiceMock, times(1)).process(testEvent);
     }
 }
--- src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -1,7 +1,11 @@
 package dev.lydtech.dispatch.service;
 
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.util.TestEventData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 class DispatchServiceTest {
 
@@ -14,6 +18,7 @@ class DispatchServiceTest {
 
     @Test
     void process() {
-        service.process("payload");
+        OrderCreated orderCreated = TestEventData.builderOrderCreatedEvent(UUID.randomUUID(), UUID.randomUUID().toString());
+        service.process(orderCreated);
     }
 }
